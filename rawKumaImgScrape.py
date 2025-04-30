a2f0449427ef8658914cdb4e2f7feed26fb76222
Psycho42069 <91679045+Psycho42069@users.noreply.github.com>
2025-04-30 19:09:07 +1000

    Making it fit the mokuro format

--- rawKumaImgScrape.py
+++ rawKumaImgScrape.py
@@ -11,14 +11,18 @@ def imgScrape(source_url: str) -> None:
     """
 
     # Manga Storage:
-    manga_folder = "mangaPages"
+    manga_folder = "のあ先輩はともだち"
     url = source_url
-    os.makedirs(manga_folder, exist_ok=True)
+    chapter_start_index = url.find("chapter")
+    chaper_end_index = url.find("/", chapter_start_index + 7)
+    chapter_number = url[chapter_start_index:chaper_end_index]
+    chapter_folder = os.path.join(manga_folder, chapter_number)
+    os.makedirs(chapter_folder, exist_ok=True)
     
     # Iterate over all files in the mangaPages folder and delete .jpg files
-    for file_name in os.listdir(manga_folder):
+    for file_name in os.listdir(chapter_folder):
         if file_name.endswith(".jpg"):
-            os.remove(os.path.join(manga_folder, file_name))
+            os.remove(os.path.join(chapter_folder, file_name))
             print(f"Deleted {file_name}")
 
     reponse = req.get(url)
@@ -38,7 +42,7 @@ def imgScrape(source_url: str) -> None:
 
     for i, a in enumerate(parts):
         image_data = req.get(a).content
-        filepath = os.path.join(manga_folder, f"{i}.jpg")
+        filepath = os.path.join(chapter_folder, f"{i}.jpg")
         with open(filepath, "wb") as file:
             file.write(image_data)
         print(f"Image {i} saved as {filepath}")
